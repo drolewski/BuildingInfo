9b7d6078b96ff739b284cd6fd27d72e49460b764
Dominik Rolewski <drolewski@gmail.com>
2019-11-05 17:20:34 +0100

    Create SpringBoot project.

---
+++ src/main/java/com/put/buildinginfo/BuildinginfoApplication.java
@@ -0,0 +1,14 @@
+package com.put.buildinginfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BuildinginfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildinginfoApplication.class, args);
+	}
+
+}
