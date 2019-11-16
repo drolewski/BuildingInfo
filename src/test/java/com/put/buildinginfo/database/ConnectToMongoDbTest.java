73578d44fa818fcf16d75c69f9b1cdd51220fb31
Dominik Rolewski <drolewski@gmail.com>
2019-11-17 00:39:32 +0100

    Add MongoDB to the project.

---
+++ src/test/java/com/put/buildinginfo/database/ConnectToMongoDbTest.java
@@ -0,0 +1,39 @@
+package com.put.buildinginfo.database;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.UnknownHostException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+
+@SpringBootTest
+public class ConnectToMongoDbTest {
+    @Test
+    public void mongoDBClientTest() throws UnknownHostException {
+        MongoClient mongoClient = new MongoClient();
+        DB newDb = mongoClient.getDB("NewTest");
+        assertThat(newDb, is(notNullValue()));
+    }
+
+    @Test(expected = Exception.class)
+    public void shouldNotBeAbleToUseMongoDBClientAfterClose() throws UnknownHostException{
+        MongoClient mongoClient = new MongoClient();
+        mongoClient.close();
+        mongoClient.getDB("SomeDatabase").getCollection("coll").insert(new BasicDBObject("field", "value"));
+    }
+
+    @Test
+    public void shouldTakeCollectionFromDatabase() throws Exception{
+        MongoClient mongoClient = new MongoClient();
+        DB newDb = mongoClient.getDB("NewTest");
+        DBCollection collection = newDb.getCollection("newCollectionTest");
+        assertThat(collection, is(notNullValue()));
+    }
+}
