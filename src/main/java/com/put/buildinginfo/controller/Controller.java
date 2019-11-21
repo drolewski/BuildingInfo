3d585d7d6d20f18f765484c4b11e33505952b6fc
mateuszratajczak <mratajczak.it@gmail.com>
2019-11-21 12:33:15 +0100

    Add Logger to Controller.

--- src/main/java/com/put/buildinginfo/controller/Controller.java
+++ src/main/java/com/put/buildinginfo/controller/Controller.java
@@ -3,6 +3,8 @@ package com.put.buildinginfo.controller;
 import com.put.buildinginfo.service.BuildingServiceImpl;
 import com.put.buildinginfo.service.FloorServiceImpl;
 import com.put.buildinginfo.service.RoomServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    private final Logger logger = LoggerFactory.getLogger(Controller.class);
+
     private final RoomServiceImpl roomService;
     private final FloorServiceImpl floorService;
     private final BuildingServiceImpl buildingService;
@@ -23,6 +27,7 @@ public class Controller {
 
     @EventListener(ApplicationReadyEvent.class)
     public void init(){
+        logger.info("Clear MongoDB collection.");
         roomService.deleteAll();
         floorService.deleteAll();
         buildingService.deleteAll();
