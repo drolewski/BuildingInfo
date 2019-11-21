ced4b4ada771e2ad565c5c21b759035ecd17c489
mateuszratajczak <mratajczak.it@gmail.com>
2019-11-21 12:50:56 +0100

    Add Logger to LevelController.

--- src/main/java/com/put/buildinginfo/controller/LevelController.java
+++ src/main/java/com/put/buildinginfo/controller/LevelController.java
@@ -4,6 +4,8 @@ import com.put.buildinginfo.applicationArchitecture.Level;
 import com.put.buildinginfo.applicationArchitecture.Room;
 import com.put.buildinginfo.exception.ImmovableNotFound;
 import com.put.buildinginfo.service.FloorServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,21 +14,23 @@ import java.util.ArrayList;
 @RestController
 @RequestMapping("level")
 public class LevelController {
+    private final Logger logger = LoggerFactory.getLogger(LevelController.class);
+
     private static final String LEVEL_NOT_FOUND = "Level not found: ";
     private final FloorServiceImpl floorService;
 
     @Autowired
-    public LevelController(FloorServiceImpl floorService) {
-        this.floorService = floorService;
-    }
+    public LevelController(FloorServiceImpl floorService) { this.floorService = floorService; }
 
     @GetMapping("")
     public ArrayList<Level> getAllFloors() {
+        logger.info("[GET] - /level");
         return floorService.getAllFloors();
     }
 
     @GetMapping("/{id}")
     public Level getFloorById(@PathVariable int id) {
+        logger.info("[GET] - /level/{id}");
         Level levelTmp = floorService.getFloorById(id);
         if(levelTmp != null) {
             return levelTmp;
@@ -36,6 +40,7 @@ public class LevelController {
 
     @GetMapping("/surface/{id}")
     public float calculateSurface(@PathVariable int id) {
+        logger.info("[GET] - /level/surface/{id}");
         float levelTop = floorService.calculateSurface(id);
         if(levelTop != -1f) {
             return levelTop;
@@ -45,6 +50,7 @@ public class LevelController {
 
     @GetMapping("/cubature/{id}")
     public float calculateCubature(@PathVariable int id) {
+        logger.info("[GET] - /level/cubature/{id}");
         float levelTmp = floorService.calculateCubature(id);
         if(levelTmp != -1f) {
             return levelTmp;
@@ -54,6 +60,7 @@ public class LevelController {
 
     @GetMapping("/lighting/{id}")
     public float calculateLighting(@PathVariable int id) {
+        logger.info("[GET] - /level/lighting/{id}");
         float levelTmp =  floorService.calculateLighting(id);
         if(levelTmp != -1f) {
             return levelTmp;
@@ -63,6 +70,7 @@ public class LevelController {
 
     @GetMapping("/heating/{id}")
     public float calculateHeating(@PathVariable int id) {
+        logger.info("[GET] - /level/heating/{id}");
         float levelTmp = floorService.calculateHeating(id);
         if(levelTmp != -1f) {
             return levelTmp;
@@ -72,16 +80,19 @@ public class LevelController {
 
     @PostMapping("")
     public Level saveNewFloor(@RequestBody Level level) {
+        logger.info("[POST] - /level");
         return floorService.saveNewFloor(level);
     }
 
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable int id)  {
+        logger.info("[DELETE] - /level/{id}");
         floorService.deleteById(id);
     }
 
     @PutMapping("")
     public Level updateFloor(@RequestBody Level level) {
+        logger.info("[PUT] - /level");
         return floorService.updateFloor(level);
     }
 
