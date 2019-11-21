ba7d623373f68a626584ca5e2b916bda2301b3f5
mateuszratajczak <mratajczak.it@gmail.com>
2019-11-21 12:25:08 +0100

    Implement initalization of WebService.

--- src/main/java/com/put/buildinginfo/controller/Controller.java
+++ src/main/java/com/put/buildinginfo/controller/Controller.java
@@ -1,10 +1,30 @@
 package com.put.buildinginfo.controller;
 
+import com.put.buildinginfo.service.BuildingServiceImpl;
+import com.put.buildinginfo.service.FloorServiceImpl;
+import com.put.buildinginfo.service.RoomServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @EventListener
+    private final RoomServiceImpl roomService;
+    private final FloorServiceImpl floorService;
+    private final BuildingServiceImpl buildingService;
+
+    @Autowired
+    private Controller(RoomServiceImpl roomService, FloorServiceImpl floorService, BuildingServiceImpl buildingService){
+        this.roomService = roomService;
+        this.floorService = floorService;
+        this.buildingService = buildingService;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init(){
+        roomService.deleteAll();
+        floorService.deleteAll();
+        buildingService.deleteAll();
+    }
 }
