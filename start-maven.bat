28c5a5450feb65232b7691a8fa58dd2c5d8de80a
Szymon <szymonrozynek98@wp.pl>
2019-12-03 12:28:27 +0100

    Maven script

---
+++ start-maven.bat
@@ -0,0 +1,8 @@
+@echo off
+set JAVA_HOME=C:\Program Files\Java\jdk-11.0.4
+set MAVEN_HOME=C:\Program Files\apache-maven-3.6.2
+set PATH=%MAVEN_HOME%\bin;%PATH%
+@echo on
+@echo "You are ready to use Maven :)."
+@echo off
+cmd /k
