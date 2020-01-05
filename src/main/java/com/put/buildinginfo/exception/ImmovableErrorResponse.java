9e74177c6562edf605dfb3f960e68f618e726300
Dominik Rolewski <drolewski@gmail.com>
2020-01-05 23:18:09 +0100

    Implement JavaDoc to the Exception classes.

--- src/main/java/com/put/buildinginfo/exception/ImmovableErrorResponse.java
+++ src/main/java/com/put/buildinginfo/exception/ImmovableErrorResponse.java
@@ -1,40 +1,83 @@
 package com.put.buildinginfo.exception;
 
+/**
+ * The type Immovable error response.
+ */
 public class ImmovableErrorResponse {
 
     private int status;
     private String message;
     private long timeStamp;
 
+    /**
+     * Instantiates a new Immovable error response.
+     */
     public ImmovableErrorResponse() {
     }
 
+    /**
+     * Instantiates a new Immovable error response.
+     *
+     * @param status    the status
+     * @param message   the message
+     * @param timeStamp the time stamp
+     */
     public ImmovableErrorResponse(int status, String message, long timeStamp) {
         this.status = status;
         this.message = message;
         this.timeStamp = timeStamp;
     }
 
+    /**
+     * Gets status.
+     *
+     * @return the status
+     */
     public int getStatus() {
         return status;
     }
 
+    /**
+     * Sets status.
+     *
+     * @param status the status
+     */
     public void setStatus(int status) {
         this.status = status;
     }
 
+    /**
+     * Gets message.
+     *
+     * @return the message
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Sets message.
+     *
+     * @param message the message
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     * Gets time stamp.
+     *
+     * @return the time stamp
+     */
     public long getTimeStamp() {
         return timeStamp;
     }
 
+    /**
+     * Sets time stamp.
+     *
+     * @param timeStamp the time stamp
+     */
     public void setTimeStamp(long timeStamp) {
         this.timeStamp = timeStamp;
     }
