1797d570907588152986767f483d522a30525c03
Dominik Rolewski <drolewski@gmail.com>
2020-01-05 22:46:22 +0100

    Room Tests.

---
+++ src/test/java/com/put/buildinginfo/applicationArchitecture/RoomTest.java
@@ -0,0 +1,36 @@
+package com.put.buildinginfo.applicationArchitecture;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RoomTest {
+
+    Room room;
+    @Before
+    public void setUp(){
+        room = new Room(1, "test", 25.6f, 100.3f, 150f, 300f);
+    }
+
+    @Test
+    public void calculateCubatureTest(){
+        assertEquals(room.calculateCubature(), room.getCubature());
+    }
+
+    @Test
+    public void calculateSurfaceTest(){
+        assertEquals(room.calculateSurface(), room.getSurface());
+    }
+
+    @Test
+    public void calculateHeatingTest(){
+        assertEquals(room.calculateHeating(), room.getHeating());
+    }
+
+    @Test
+    public void calculateLightingTest(){
+        assertEquals(room.calculateLighting(), room.getLighting());
+    }
+
+}
